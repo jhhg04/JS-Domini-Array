bf69bf1f161e61f07a93de16eabe8e774ac42d2f
jhhg0 <jhherrera@ccss.sa.cr>
2021-09-16 11:20:32 -0500

    first commit

---
+++ Array/arrayMake.js
@@ -0,0 +1,11 @@
+// const cities = [];
+// const cities = Array.of('Madrid', 'Valencia', 'Lugo');
+const cities = ['Madrid', 'Valencia', 'Lugo'];
+console.log(cities);
+
+// console.log(typeof cities); // lo reconoce como un objeto
+// console.log(Array.isArray(cities)); // devuelve true si es array
+
+cities[1] = 'Barcelona'; // Modifica posicion del array
+console.log(cities);
+console.log(cities[2]); // DEvuelve una  posicion del array
