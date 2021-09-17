e2511bb0698db1106d625310b5b0186348f849b8
jhhg0 <jhherrera@ccss.sa.cr>
2021-09-17 17:39:21 -0500

    add

---
+++ Array/arrayReduce.js
@@ -0,0 +1,12 @@
+// El método reduce() ejecuta una función reductora sobre cada elemento de un array, devolviendo como resultado un único valor.
+
+const array1 = [1, 2, 3, 4];
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
+
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
